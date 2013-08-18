3530b1c65d6366642b93459321fb788f7571e7e3
Garry M. Perez <perez.garrym@gmail.com>
2013-08-18 18:29:52 +0800

    changes

--- app/Config/database.php
+++ app/Config/database.php
@@ -65,7 +65,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'barbra',
+		'database' => 'shop',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
